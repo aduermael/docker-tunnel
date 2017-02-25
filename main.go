ebb210a3bc0f90aa169e9be75aabcd0b19af635d
Adrien Duermael <adrien@duermael.com>
2017-02-25 01:39:08 -0800

    proxy mode
    Signed-off-by: Adrien Duermael <adrien@duermael.com>

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
 	"log"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -15,7 +20,10 @@ import (
 var (
 	// path to private key or empty to use default
 	sshIdentityFile = ""
-	shell           = "bash"
+	// open a bash session by default, but different option can be used
+	shell = "bash"
+	// proxy mode (don't start shell session)
+	proxyMode = false
 )
 
 func main() {
@@ -38,6 +46,18 @@ func main() {
 			defer tunnelProcess.Kill()
 			defer os.RemoveAll(socketPath)
 
+			if proxyMode {
+				fmt.Println("listening on port 2375")
+				err := http.ListenAndServe(":2375", proxy(socketPath))
+				if err != nil {
+					log.Fatalln(err)
+				}
+				return
+			}
+
+			// proxyMode == false
+			// open shell session, connected to remote Docker host
+
 			os.Setenv("PS1", "🐳  $ ")
 			os.Setenv("DOCKER_HOST", "unix://"+socketPath)
 
@@ -55,6 +75,7 @@ func main() {
 
 	rootCmd.Flags().StringVarP(&sshIdentityFile, "sshid", "i", "", "path to private key")
 	rootCmd.Flags().StringVarP(&shell, "shell", "s", "bash", "shell to open session")
+	rootCmd.Flags().BoolVarP(&proxyMode, "proxy", "p", false, "proxy mode (don't start shell session)")
 
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatalln(err.Error())
@@ -92,4 +113,22 @@ func tmpSocketPath() string {
 	randBytes := make([]byte, 16)
 	rand.Read(randBytes)
 	return filepath.Join(os.TempDir(), "docker-"+hex.EncodeToString(randBytes)+".sock")
+}
+
+func proxy(socketPath string) *httputil.ReverseProxy {
+	u, err := url.Parse("http://unix.sock")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(u)
+
+	tr := &http.Transport{
+		Dial: func(proto, addr string) (conn net.Conn, err error) {
+			return net.Dial("unix", socketPath)
+		},
+	}
+	proxy.Transport = tr
+
+	return proxy
 }
